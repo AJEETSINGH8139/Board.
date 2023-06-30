dec7d3c54deedc4db4808cb96f35e77eec272780
AJEET SINGH <ajeet.singh.cs23@iilmcet.ac.in>
2023-06-30 20:32:57 +0530

    Now It's ready to host

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Registration from "./components/Registration";
 import SignIn from "./components/SignIn";
 import PageNotFound from "./components/PageNotFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
 
 import { auth } from "./firebase";
 function App() {
@@ -20,13 +22,22 @@ function App() {
   return (
     <div className="App">
       <Router>
+      <UserAuthContextProvider>
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/registration" element={<Registration />} />
-          <Route path="/home" element={<Home />} />
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
+        </UserAuthContextProvider>
       </Router>
     </div>
   );
